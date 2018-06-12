db9e0afd21cda90e17f98a9586562e2d6d229597
IgnacioPasamontes <ignacio.pasamontes@upf.edu>
2018-06-12 16:41:56 +0200

    TreeView

--- src/app/quantitative-table/quantitative-table.component.ts
+++ src/app/quantitative-table/quantitative-table.component.ts
@@ -1,37 +1,39 @@
 import { Component, ViewContainerRef } from '@angular/core';
 import { ModalDialogService, SimpleModalComponent } from 'ngx-modal-dialog';
 import {TooltipModule} from "ngx-tooltip";
-import { TreeviewItem, TreeviewConfig, TreeItem } from 'ngx-treeview';
+import {
+    TreeviewI18n, TreeviewItem, TreeviewConfig, TreeviewHelper, TreeviewComponent,
+TreeviewEventParser, DownlineTreeviewEventParser, DownlineTreeviewItem} from 'ngx-treeview';
+import { isNull } from 'util';
 
 @Component({
   selector: 'app-quantitative-table',
   templateUrl: './quantitative-table.component.html',
-  styleUrls: ['./quantitative-table.component.css']
+  styleUrls: ['./quantitative-table.component.css'],
+ providers: [
+    { provide: TreeviewEventParser, useClass: DownlineTreeviewEventParser }
+]
 })
 export class QuantitativeTableComponent{
 
-  dropdownEnabled = true;
+  
   items: TreeviewItem[];
   values: number[];
   config = TreeviewConfig.create({
-     // hasAllCheckBox: true,
+     hasAllCheckBox: false,
       hasFilter: true,
-      //hasCollapseExpand: true,
-      decoupleChildFromParent: false,
-      maxHeight: 400
+      maxHeight: 400   
   });
   buttonClass = 'btn-outline-secondary';
+  rows={}
+  objectKeys = Object.keys;
 
   constructor() { }
 
   ngOnInit() {
-
     let organs ={"hola":{},"adios":{},"liver":{"liver1":{"liver11":{},"liver12":{"liver121":{},"liver122":{}}},"liver2":{}},
              "lunge":{}}
      this.items=this.createTreeview(organs);   
-     
-    
- 
  }
  
 
@@ -43,11 +45,11 @@ export class QuantitativeTableComponent{
     for (var key in organs) {   
         if (Object.keys(organs[key]).length>0){    
             this.createTreeview(organs[key])
-            item = new TreeviewItem({ text:key, value:key, checked: false,children: this.createTreeview(organs[key])})
+            item = new TreeviewItem({ text:key, value:key, collapsed: true, checked: false,children: this.createTreeview(organs[key])})
             items.push(item);
         }
         else{
-            item = new TreeviewItem({ text:key, value:key, checked: false})
+            item = new TreeviewItem({ text:key, value:key, collapsed: true,checked: false})
             items.push(item);
         }      
     }  
@@ -56,7 +58,19 @@ export class QuantitativeTableComponent{
 
 
 
-  onFilterChange(value: string) {
-      console.log('filter:', value);
-  }
+  onFilterChange(downlineItems: DownlineTreeviewItem[]) {
+
+    this.rows = {};
+        downlineItems.forEach(downlineItem => {   
+            const item = downlineItem.item;
+            this.rows[item.text]=true;
+            let parent = downlineItem.parent;
+           while (!isNull(parent) && parent.item.checked) {
+                this.rows[parent.item.text]=true;
+                parent = parent.parent;
+            }
+        });
+        
+    
+    }
 }
