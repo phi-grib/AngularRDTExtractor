f27863c76b9c7ca571fea1d392beaa5d2b67ac86
bet-gregori <elisabet.gregori@gmail.com>
2018-06-22 14:51:30 +0200

    Cleated a plot tab for #16

---
+++ src/app/plot/plot.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input,ViewChildren,QueryList,ElementRef,AfterViewInit ,ViewContainerRef, Renderer2} from '@angular/core';
+import { HttpClient,HttpParams } from '@angular/common/http';
+import { FindingsService } from '../findings.service';
+import * as SmilesDrawer from 'smiles-drawer';
+import { ModalDialogService, SimpleModalComponent } from 'ngx-modal-dialog';
+import { CustomModalComponent } from '../dialog/dialog.component';
+
+
+@Component({
+  selector: 'app-plot',
+  templateUrl: './plot.component.html',
+  styleUrls: ['./plot.component.css']
+})
+export class PlotComponent implements OnInit,AfterViewInit {
+
+  table_info = {};
+  @Input() searchFormTable = {};
+  @ViewChildren('cmp') components:QueryList<ElementRef>;
+
+  constructor(private findService : FindingsService, 
+    private modalDialogService: ModalDialogService, 
+    private viewContainer: ViewContainerRef,
+    private renderer: Renderer2) {}
+
+  ngOnInit() {
+    this.findService.currentTable.subscribe (table_info => this.table_info = table_info);
+  }
+}
