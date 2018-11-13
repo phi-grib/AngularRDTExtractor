4ea66c1f2e15219db786843ddcca01c8157e21e3
Ignacio Pasamontes <36951846+IgnacioPasamontes@users.noreply.github.com>
2018-11-13 16:02:49 +0100

    Export change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,18 @@ import { FindingsService } from './findings.service';
 })
 export class AppComponent {
   title = 'RDTExtractor';
+  doseDisabled:boolean=true
+  mindose: number;
 
   constructor(private findService : FindingsService) {}
   
   download() { 
-    this.findService.downloadFiles();
+    alert(this.mindose)
+    //this.findService.downloadFiles();
+  }
+  enableDose(){
+    this.mindose=null
+    this.doseDisabled = this.doseDisabled ? false : true;
   }
 }
 
